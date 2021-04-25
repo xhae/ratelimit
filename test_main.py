862532ec4139193cb532b9aa3e64cf905f8dbf0d
Hyeonjong Ryu <xoancer@gmail.com>
2021-04-26 02:21:29 +0900

    Test for time duration added

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 import main
@@ -6,6 +8,7 @@ import main
 @pytest.fixture
 def client():
 	main.app.config['TESTING'] = True
+	main.REQUEST_TIMEBOUND = 10
 	with main.app.test_client() as client:
 		yield client
 
@@ -15,15 +18,24 @@ def test_success(client):
 	assert b'hello' in rv.data
 
 
-def test_success_100_times(client):
-	for i in range(100):
+def test_success_maximum_limitation_times(client):
+	for i in range(main.MAXIMUM_REQUEST_PER_WINDOW):
 		rv = client.get('/')
 		assert b'hello' in rv.data
 
 
-def test_fail_101_times(client):
-	for i in range(100):
+def test_fail_over_maximum_limitation_times(client):
+	for i in range(main.MAXIMUM_REQUEST_PER_WINDOW):
 		rv = client.get('/')
 		assert b'hello' in rv.data
 	rv = client.get('/')
 	assert b'Rate limit' in rv.data
+
+
+def test_success_over_maximum_after_time_window(client):
+	for i in range(main.MAXIMUM_REQUEST_PER_WINDOW):
+		rv = client.get('/')
+		assert b'hello' in rv.data
+	time.sleep(12)
+	rv = client.get('/')
+	assert b'hello' in rv.data
